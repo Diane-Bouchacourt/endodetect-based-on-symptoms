f37062bc6b796b79d540851c59334541f3a62dc0
Diane-Bouchacourt <diane.bouchacourt@gmail.com>
2025-06-12 17:55:52 +0200

    b

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,19 +1,159 @@
 import pandas as pd
+import pdb 
 
+SYMPTOMS = [
+    "Heavy / Extreme menstrual bleeding",
+    "Menstrual pain (Dysmenorrhea)",
+    "Painful / Burning pain during sex (Dyspareunia)",
+    "Pelvic pain",
+    "Irregular / Missed periods",
+    "Cramping",
+    "Abdominal pain / pressure",
+    "Back pain",
+    "Painful bowel movements",
+    "Nausea",
+    "Menstrual clots",
+    "Infertility",
+    "Painful cramps during period",
+    "Pain / Chronic pain",
+    "Diarrhea",
+    "Long menstruation",
+    "Constipation / Chronic constipation",
+    "Vomiting / constant vomiting",
+    "Fatigue / Chronic fatigue",
+    "Painful ovulation",
+    "Stomach cramping",
+    "Migraines",
+    "Extreme / Severe pain",
+    "Leg pain",
+    "Irritable Bowel Syndrome (IBS)",
+    "Syncope (fainting, passing out)",
+    "Mood swings",
+    "Depression",
+    "Bleeding",
+    "Lower back pain",
+    "Fertility Issues",
+    "Ovarian cysts",
+    "Painful urination",
+    "Headaches",
+    "Constant bleeding",
+    "Pain after Intercourse",
+    "Digestive / GI problems",
+    "IBS-like symptoms",
+    "Excessive bleeding",
+    "Anaemia / Iron deficiency",
+    "Hip pain",
+    "Vaginal Pain/Pressure",
+    "Sharp / Stabbing pain",
+    "Bowel pain",
+    "Anxiety",
+    "Cysts (unspecified)",
+    "Dizziness",
+    "Malaise / Sickness",
+    "Abnormal uterine bleeding",
+    "Fever",
+    "Hormonal problems",
+    "Bloating",
+    "Feeling sick",
+    "Decreased energy / Exhaustion",
+    "Abdominal Cramps during Intercourse",
+    "Insomnia / Sleeplessness",
+    "Acne / pimples",
+    "Loss of appetite",
+]
+
+MACROS_SYMPTOMS = {
+    "DIGESTIVE": [
+        "Painful bowel movements",
+        "Nausea",
+        "Diarrhea",
+        "Constipation / Chronic constipation",
+        "Vomiting / constant vomiting",
+        "Painful ovulation",
+        "Stomach cramping",
+        "Irritable Bowel Syndrome (IBS)",
+        "Digestive / GI problems",
+        "IBS-like symptoms",
+        "Bowel pain",
+        "Bloating",
+        "Loss of appetite",
+    ],
+    "PERIODS": [
+        "Heavy / Extreme menstrual bleeding",
+        "Menstrual pain (Dysmenorrhea)",
+        "Irregular / Missed periods",
+        "Menstrual clots",
+        "Painful cramps during period",
+        "Long menstruation",
+    ],
+    "SKIN": ["Acne / pimples"],
+    "BLOOD": [
+        "Bleeding",
+        "Constant bleeding",
+        "Excessive bleeding",
+        "Anaemia / Iron deficiency",
+        "Abnormal uterine bleeding",
+    ],
+    "SEXUAL": [
+        "Painful / Burning pain during sex (Dyspareunia)",
+        "Pain after Intercourse",
+        "Abdominal Cramps during Intercourse",
+    ],
+    "MSK": ["Cramping", "Back pain", "Leg pain", "Lower back pain", "Hip pain"],
+    "MENTAL HEALTH": ["Mood swings", "Depression"],
+    "HORMONAL": ["Hormonal problems"],
+    "OTHER PAINS": [
+        "Pelvic pain",
+        "Abdominal pain / pressure",
+        "Pain / Chronic pain",
+        "Extreme / Severe pain",
+        "Painful urination",
+        "Vaginal Pain/Pressure",
+        "Sharp / Stabbing pain",
+        "Cysts (unspecified)",
+    ],
+    "REPRODUCTIVE": [
+        "Infertility",
+        "Fertility Issues",
+        "Ovarian cysts",
+    ],
+    "SICKNESS / ENERGY": [
+        "Fatigue / Chronic fatigue",
+        "Migraines",
+        "Syncope (fainting, passing out)",
+        "Headaches",
+        "Dizziness",
+        "Malaise / Sickness",
+        "Fever",
+        "Feeling sick",
+        "Decreased energy / Exhaustion",
+        "Insomnia / Sleeplessness",
+    ],
+}
 
 def prepare_data():
     data = pd.read_excel("../data/dataset.xlsx")
 
     y = data["label"].values
     data = data.drop(data.columns[0], axis=1)  # remove first columns
-    data = data.drop(["row", "label"], axis=1)
+    data = data.drop(["row"], axis=1)
+    # remove symptoms with less than 20 datapoints 
+    for column in data.columns:
+        if data[column].sum() < 20:
+            data = data.drop([column])
     return data
 
 
 def create_macros_symptoms(data):
-    pass
+    
+    data = data.drop(columns=["label"])
+    df = data.copy()
+    for macro_symptom, ls_symptoms in MACROS_SYMPTOMS.items():
+        df[macro_symptom] = (data[ls_symptoms].sum(1)>0).astype(int)
+    return df     
 
 
 if __name__ == "__main__":
 
     data = prepare_data()
+    create_macros_symptoms(data)
--- src/pca_analysis.py
+++ src/pca_analysis.py
@@ -1,30 +1,49 @@
 import pandas as pd
 from sklearn.decomposition import PCA
+import matplotlib
 import matplotlib.pyplot as plt
+import data_processing
+from data_processing import MACROS_SYMPTOMS
+import pdb 
 
 if __name__ == "__main__":
 
-    data = pd.read_excel("../data/dataset.xlsx")
+    data = data_processing.prepare_data()
+    macro_data = data_processing.create_macros_symptoms(data) # label is already removed
 
     y = data["label"].values
-    data = data.drop(data.columns[0], axis=1)  # remove first columns
-    data = data.drop(["row", "label"], axis=1)
+    data = data.drop(["label"], axis=1)
     pca = PCA(n_components=10)
     pca.fit(data)
     X_t = pca.transform(data)
 
     print("explained variance ratio : %s" % str(pca.explained_variance_ratio_))
 
-    colors = ["navy", "turquoise"]
+    colors = ['green','red']
 
-    plt.figure()
+    for s in MACROS_SYMPTOMS:
+        plt.figure()
+        for color, label in zip(colors, [0,1]):
+            y2 = macro_data[s].values
+            symptomatic = ((y == label) & (y2 == 1))
+            asymptomatic = ((y == label) & (y2 == 0))  
+            plt.scatter(
+                X_t[symptomatic, 0],
+                X_t[symptomatic, 1],
+                marker='+',
+                color = color,
+                alpha=0.5,
+                label = label
+            )
+            plt.scatter(
+                X_t[asymptomatic, 0],
+                X_t[asymptomatic, 1],
+                marker='s',
+                color = color,
+                alpha=0.5,
+            )
+        plt.title(s)
+        plt.legend()
+        plt.show()
 
-    for color, i in zip(colors, [0, 1]):
-        plt.scatter(
-            X_t[y == i, 0],
-            X_t[y == i, 1],
-            color=color,
-            alpha=0.8,
-        )
-
-    plt.show()
+ 
